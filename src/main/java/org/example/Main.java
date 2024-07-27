40db465eddd7481ac2f40372cc174556ea8f1378
Psytex <gigaface3@gmail.com>
2024-07-27 10:07:23 +0200

    Discord Bot

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,29 @@
+package org.example;
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.intent.Intent;
+;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void main(String[] args) {
+        // Log the bot in
+        DiscordApi api = new DiscordApiBuilder()
+                .setToken("TOKEN")
+                .setAllIntents()
+                .login().join();
+
+        // Add a listener which answers with "Pong!" if someone writes "!ping"
+        api.addMessageCreateListener(event -> {
+            if (event.getMessageContent().equalsIgnoreCase("!ping")) {
+                event.getChannel().sendMessage("Pong!");
+            }
+        });
+
+        // Print the invite url of your bot
+        System.out.println("You can invite the bot by using the following url: " + api.createBotInvite());
+    }
+
+}
